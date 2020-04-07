eb6b0452f38e038bccef6cb2eb9a100456e16dc9
dreamdroiddev <dreamdroiddev@gmail.com>
2020-04-07 20:20:40 +0600

    same

--- app/src/main/java/com/example/ac_twitterclone/WelcomeActivity.java
+++ app/src/main/java/com/example/ac_twitterclone/WelcomeActivity.java
@@ -1,22 +1,33 @@
 package com.example.ac_twitterclone;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.parse.ParseUser;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     ParseUser mUser;
     private TextView txtWelcomeMessage;
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         txtWelcomeMessage = findViewById(R.id.txtWelcomeMessage);
-        txtWelcomeMessage.setText("Welcome!");
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(this);
+        txtWelcomeMessage.setText("Welcome "+ParseUser.getCurrentUser().getUsername());
+    }
+
+    @Override
+    public void onClick(View v) {
+        ParseUser.logOut();
+        finish();
     }
 }
